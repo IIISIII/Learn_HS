a5b1a878a243d4cf1a021df60847ddc527c07521
lllSlll <sunny90224@gmail.com>
2022-11-13 19:30:08 +0900

    start with 'yarn dev'

--- src/components/CrawlerTest.js
+++ src/components/CrawlerTest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 
-const url = "https://crawler-hansung.herokuapp.com/crawl";
+const url = "/api/crawl";
 
 const getData = (params) => {
     return axios.post(url, params);
@@ -11,13 +11,22 @@ const CrawlingTest = () => {
     const [id, setId] = useState("");
     const [password, setPassword] = useState("");
     const [data, setData] = useState();
+    const [loading, setLoaing] = useState(false);
 
     const login = (e) => {
         e.preventDefault();
 
+        const params = { uid: id, upw: password };
+
         console.log("wait for server response");
 
-        
+        setLoaing(true);
+
+        getData(params)
+            .then(res => JSON.stringify(res, null, 2))
+            .then(setData)
+            .then(() => setLoaing(false))
+            .catch(console.error);
     }
 
     return (
@@ -25,6 +34,7 @@ const CrawlingTest = () => {
             <input type="text" name="uid" onChange={ e => setId(e.target.value) }/>
             <input type="password" name="upw" onChange={ e => setPassword(e.target.value) }/>
             <button onClick={ login }>login</button>
+            { loading && <p>Loading...</p> }
             { data && <pre>{ data }</pre> }
         </>
     );
